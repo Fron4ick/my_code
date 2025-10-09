42c9073447c081e68de01903be88e85e90552fb8
Fron <f_rozhin123@mail.ru>
2025-10-10 00:45:13 +0500

    s

--- Ventyls/disp/Mux.py
+++ Ventyls/disp/Mux.py
@@ -1,16 +1,20 @@
 def Mux(s):
-    a, b, sel, out = map(str, s.replace('	Mux(a=', '').replace(', b=', ' ').replace(', sel=', ' ').replace(', out=', ' ').replace(');', '').strip().split())
-    print(f'	Not(in={sel}, out=n{sel});')
-    print(int(out[out.index('[')+1:-2]))
-    #if out[-1]==']':
-        #for i in range(out[out.index('[')])
+    a, b, sel, out = map(str, s.replace('Mux(a=', '').replace(', b=', ' ').replace(', sel=', ' ').replace(', out=', ' ').replace(')', '').strip().split())
+    outs = [f'Not(in={sel}, out=n{sel});']
+    kolvo = int(out[out.index('[')+1:-1] if )
+    print(kolvo)
+    al = a[-1]==']'
+    bl = b[-1]==']'
+    ol = out[-1]==']'
+    a = a[:a.index('[')]
+    b = b[:b.index('[')]
+    out = out[:out.index('[')]
+    for i in range(kolvo):
+        outs.append(f'Nand(a={a}{al*'[' + (not al)*'X'}{i}{al*']'}, b=n{sel}, out={a}X{i}Xn{sel})')
+        outs.append(f'Nand(a={b}{bl*'[' + (not bl)*'X'}{i}{bl*']'}, b={sel}, out={b}X{i}X{sel})')
+        outs.append(f'Nand(a={a}X{i}Xn{sel}, b={b}X{i}X{sel}, out={out}{ol*'[' + (not ol)*'X'}{i}{ol*']'})')
+    return outs
 
-	#Not(in=sel, out=nsel);
-	#Nand(a=a, b=nsel, out=aXnsel);
-	#Nand(a=b, b=sel, out=bXsel);
-	#Nand(a=aXnsel, b=bXsel, out=aXnselXbXsel);
-#Mux('	Mux(a=a, b=b, sel=sel, out=out[16])')
+p = list(Mux('Mux(a=a, b=b, sel=sel, out=out)'))
 
-
-print( ('['=='[') * '[',  ('['==']') * '[' )
-print( ('['=='[') *  5 ,  ('['==']') *  5  )
+for i in p:print('	', i, ';', sep='')
